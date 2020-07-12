31a4156fdf39428345e454741590c56cceb653df
吴之宇 <1002207631@qq.com>
2020-07-12 19:52:28 +0800

    吴之宇
    修改

--- pages/member/add-friend/add-friend.vue
+++ pages/member/add-friend/add-friend.vue
@@ -2,27 +2,27 @@
 	<view style="background-color: #f0eff3;height: 100%;">
 		<navbar title="添加好友" :backBtn="true"></navbar>
 		<view class="content">			
-			<button>
+			<view class="list-go">
 				<view class="img-text">					
 					<image class="contact" src="@/static/icon/member/contact.png" mode=""></image>
 					<p>手机联系人</p>
 				</view>
 				<image class="arrows" src="@/static/icon/member/arrows.png" mode=""></image>
-			</button>
-			<button>
+			</view>
+			<view class="list-go">
 				<view class="img-text">
 					<image class="scan" src="@/static/icon/member/scan.png" mode=""></image>
 					<p>扫一扫</p>
 				</view>
 				<image class="arrows" src="@/static/icon/member/arrows.png" mode=""></image>
-			</button>
-			<button>
+			</view>
+			<view class="list-go">
 				<view class="img-text">
 					<image class="near" src="@/static/icon/member/nearpeople.png" mode=""></image>
 					<p>附近的人</p>
 				</view>
 				<image class="arrows" src="@/static/icon/member/arrows.png" mode=""></image>
-			</button>
+			</view>
 		</view>
 	</view>
 </template>
@@ -40,11 +40,14 @@
 <style lang="scss">
 .content {
 	margin: 15px 0 0 0;
-	button {
+	.list-go {
 		display: flex;
-		width: 375px;
 		height: 44px;
 		margin: 0 0 8px 0;
+		border-top: 1px solid #dddddd;
+		border-bottom: 1px solid #dddddd;
+		padding:0 15px;
+		background-color: #FFFFFF;
 		.img-text{
 			display: flex;
 			flex-direction: row;
@@ -73,6 +76,9 @@
 			width: 6.5px;
 			height: 12px;			
 		}
+	}
+	.list-go:active{
+		background-color: #e8e8e8;
 	}
 }
 </style>
